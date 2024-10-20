3d23f8ea7cc4632aeeeb33731475f3d43abdac01
Rabby 5264 <nuruddinrabby83@gmail.com>
2024-10-20 23:49:48 +0600

    contact page frontend done

--- resources/views/admin/contact/contact.blade.php
+++ resources/views/admin/contact/contact.blade.php
@@ -8,19 +8,117 @@
     <div class="card">
         <div class="card-body">
           <h5 class="card-title fw-semibold mb-4">Contact Massage</h5>
-          <p class="mb-0">This is a sample page </p>
-
-
+            
+                <div style="overflow-y:scroll; height:200px;" class="p-2">
+                  <table class="table table-bordered border-primary">
+                      <thead class="bg-light sticky-top">
+                          <tr class="position-relative text-center">
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Massage</th>
+                            <th scope="col">Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th>1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>
+                              <div class="d-flex justify-content-end"> 
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#75FB4C"><path d="m381-240 424-424-57-56-368 367-169-170-57 57 227 226Zm0 113L42-466l169-170 170 170 366-367 172 168-538 538Z"/></svg>
+                              </div>                 
+                            </td>
+                          </tr>
+                          <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>
+                              <div class="d-flex justify-content-end"> 
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#75FB4C"><path d="m381-240 424-424-57-56-368 367-169-170-57 57 227 226Zm0 113L42-466l169-170 170 170 366-367 172 168-538 538Z"/></svg>
+                              </div> 
+                            </td>
+                          </tr>
+                          <tr>
+                              <th scope="row">3</th>
+                              <td>Jacob</td>
+                              <td>Thornton</td>
+                              <td>
+                                <div class="d-flex justify-content-end"> 
+                                  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                                  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#75FB4C"><path d="m381-240 424-424-57-56-368 367-169-170-57 57 227 226Zm0 113L42-466l169-170 170 170 366-367 172 168-538 538Z"/></svg>
+                                </div> 
+                              </td> 
+                            </tr>
+                        </tbody>
+                    </table>
+              </div>
 
         </div>
     </div>
+
+
+
+    
     <div class="card">
       <div class="card-body">
         <div class="d-flex justify-content-between">
           <h5 class="card-title fw-semibold mb-4">Contact Info</h5>
           <a href="{{route('contact.create')}}" class="btn btn-light m-1">Add New Info</a>
         </div>
-          <p class="mb-0">This is a sample page </p>
+          
+    <div class="p-2">
+      <table class="table table-bordered border-primary">
+          <thead class="bg-light">
+              <tr class="position-relative text-center">
+                <th colspan="2" scope="col">Name</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                  <td>
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a distribution of letters.
+                  </td>
+                  <td>
+                    <div class="d-flex justify-content-end"> 
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                      <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#75FB4C"><path d="m381-240 424-424-57-56-368 367-169-170-57 57 227 226Zm0 113L42-466l169-170 170 170 366-367 172 168-538 538Z"/></svg>
+                    </div>               
+                    </td>
+              </tr>
+            </tbody>
+        </table>
+  </div>
+
+  <h5>Outlet</h5>
+  <div class="p-2">
+      <table class="table table-bordered border-primary">
+          <thead class="bg-light">
+              <tr class="position-relative text-center">
+                <th scope="col"> Office Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Adress</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>    
+                  <div class="d-flex justify-content-end"> 
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#EA3323"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#75FB4C"><path d="m381-240 424-424-57-56-368 367-169-170-57 57 227 226Zm0 113L42-466l169-170 170 170 366-367 172 168-538 538Z"/></svg>
+                  </div>                   
+                </td>
+            </tbody>
+        </table>
+  </div>
+  
 
 
 
--- resources/views/admin/contact/create.blade.php
+++ resources/views/admin/contact/create.blade.php
@@ -10,8 +10,48 @@
         <div class="d-flex justify-content-between">
           <h5 class="card-title fw-semibold mb-4">Add New Contact Info</h5>
         </div>
-          <p class="mb-0">This is a sample page </p>
+          
+            <h2>Info</h2>
+            <form action="">
+                <div class="d-flex">
+                    <div class="col-md-7 p-2">
+                        <input type="text" class="form-control" required>
+                    </div>
+                    <div class="col-md-3 p-2">
+                        <button class="btn btn-primary" type="submit">Update</button>
+                    </div>
+                </div>
+            </form>
 
+
+
+        <h2>outlet</h2>
+
+        <form>
+            <div class="col-md-6 d-flex p-3">
+              <label for="validationDefault03" class="form-label col-md-2"><h5>City  </h5></label>
+              <input type="text" class="form-control" id="validationDefault03" required>
+            </div>
+            
+            <div class="col-md-6 d-flex p-2">
+                <label for="validationDefault03" class="form-label col-md-2"><h5>Address </h5></label>
+                <input type="text" class="form-control" id="validationDefault03" required>
+              </div>
+              
+            <div class="col-md-6 d-flex p-2">
+                <label for="validationDefault03" class="form-label col-md-2"> <h5>Email</h5></label>
+                <input type="text" class="form-control" id="validationDefault03" required>
+              </div>
+
+            <div class="col-md-6 d-flex p-2">
+              <label for="validationDefault05" class="form-label col-md-2"> <h5>Mobile</h5></label>
+              <input type="text" class="form-control" id="validationDefault05" required>
+            </div>
+
+            <div class="col-12 p-5">
+              <button class="btn btn-primary" type="submit">Add New</button>
+            </div>
+          </form>
 
 
       </div>
